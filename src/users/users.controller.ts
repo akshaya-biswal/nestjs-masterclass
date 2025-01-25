0f120bc428f88165ab26f061d3d8d4e13a3d24ea
akshaya-biswal <mraxya@gmail.com>
2025-01-25 09:50:39 +0530

    Feature: User controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  public getUsers() {
+    return 'You sent a get request to users endpoints';
+  }
+
+  @Get('/:id')
+  public getUser(@Param() params: any) {
+    return {
+      params,
+      message: 'You sent a get request to a single user endpoints',
+    };
+  }
+
+  @Post()
+  public createUsers() {
+    return 'You sent a get request to users endpoints';
+  }
+
+  public editUsers() {
+    return 'You sent a get request to users endpoints';
+  }
+}
