ea11b2294d96116740c4a19f2e7a69644d06812a
akshaya-biswal <mraxya@gmail.com>
2025-01-27 09:18:36 +0530

    Feat: User controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,33 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
+import { UsersService } from './providers/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(
+    // Injecting Users Service
+    private readonly usersService: UsersService,
+  ) {}
+
   @Get('/:id?')
-  public getUsers(@Param() getUsersParamDto: GetUsersParamDto) {
-    return getUsersParamDto;
+  public getUsers(
+    @Param() getUsersParamDto: GetUsersParamDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(10), ParseIntPipe) page: number,
+  ) {
+    return this.usersService.findAll(getUsersParamDto, limit, page);
   }
 
   @Post()
@@ -14,7 +35,8 @@ export class UsersController {
     return createUserDto;
   }
 
-  public editUsers() {
-    return 'You sent a get request to users endpoints';
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    return patchUserDto;
   }
 }
