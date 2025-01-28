b7bec7ce4c8034da35b9dd91d90296b8b399c808
akshaya-biswal <mraxya@gmail.com>
2025-01-28 10:22:35 +0530

    docs: User Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,7 +13,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
 import { UsersService } from './providers/users.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
 @ApiTags('Users')
@@ -24,6 +24,28 @@ export class UsersController {
   ) {}
 
   @Get('/:id?')
+  @ApiOperation({
+    summary: 'Fetches a list of registered users on the application.',
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: String,
+    description: 'The upper limit of pages you want the pagination to return',
+    required: false,
+    example: 10,
+  })
+  @ApiQuery({
+    name: 'page',
+    type: String,
+    description:
+      'The position of the page number that you want the API to return',
+    required: false,
+    example: 1,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Users fetched successfully based on the query',
+  })
   public getUsers(
     @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
